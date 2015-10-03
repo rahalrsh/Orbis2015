e6c3bb24121bb18198b17d96f662ee2e8d4b539b
rahalrsh <rahalrsh@gmail.com>
2015-10-03 16:14:10 -0400

    Check if emeny is close to the player

--- Java/botplayer/PlayerAI.java
+++ Java/botplayer/PlayerAI.java
@@ -2,6 +2,12 @@ import java.util.*;
 import java.awt.Point;
 
 public class PlayerAI extends ClientAI {
+	
+	int safeDistanceratio = 3;
+	boolean enemyIsClose = false;
+	
+	int dx, dy, dx_wrapped, dy_wrapped;
+	
 	public PlayerAI() {
 		//Write your initialization here
 	}
@@ -9,6 +15,28 @@ public class PlayerAI extends ClientAI {
 	@Override
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player) throws NoItemException, MapOutOfBoundsException {
 		
+		dx = Math.abs(player.x - opponent.x);
+		dy = Math.abs(player.y - opponent.y);
+		if (dx > gameboard.getWidth()/safeDistanceratio || dy > gameboard.getHeight()/safeDistanceratio){
+			  dx_wrapped = gameboard.getWidth() - dx;
+			  dy_wrapped = gameboard.getHeight() - dy;
+			  if (dx_wrapped > gameboard.getWidth()/safeDistanceratio && dx > gameboard.getWidth()/safeDistanceratio){
+				  enemyIsClose = false;
+			  }
+			  else if(dy_wrapped > gameboard.getHeight()/safeDistanceratio && dy > gameboard.getHeight()/safeDistanceratio){
+				  enemyIsClose = false;
+			  }
+			  else{
+				  enemyIsClose = true;
+			  }
+		}
+		else{
+			enemyIsClose = true;
+		}
+		
+		if(enemyIsClose)
+			System.out.println("ENEMY IS CLOSE!!!!\n");
+
 		//Write your AI here
 		return Move.NONE;
 	}
