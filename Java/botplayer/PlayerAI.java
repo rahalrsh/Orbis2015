6e4e099b73a9204f60b8cc30721367f80e06e1f2
rahalrsh <rahalrsh@gmail.com>
2015-10-03 19:47:53 -0400

    check if player is safe from Turrets

--- Java/botplayer/PlayerAI.java
+++ Java/botplayer/PlayerAI.java
@@ -46,27 +46,25 @@ public class PlayerAI extends ClientAI {
 			enemyIsClose = true;
 		}
 
-		if (enemyIsClose)
-			System.out.println("ENEMY IS CLOSE!!!!\n");
 
 		// Write your AI here
-		GameWidth = gameboard.getWidth();
-		GameHeight = gameboard.getHeight();
+		GameWidth = gameboard.getWidth()-1;
+		GameHeight = gameboard.getHeight()-1;
 
 		BulletArrayList = gameboard.getBullets();
 		PowerUpArrayList = gameboard.getPowerUps();
 		WallsArrayList = gameboard.getWalls();
 
-		for (int i = 0; i < BulletArrayList.size(); i++) {
-			System.out.println("B x :" + BulletArrayList.get(i).x + "B y: "
-					+ BulletArrayList.get(i).y);
-		}
+		//for (int i = 0; i < BulletArrayList.size(); i++) {
+		//	System.out.println("B x :" + BulletArrayList.get(i).x + "B y: "
+		//			+ BulletArrayList.get(i).y);
+		//}
 
-		for (int i = 0; i < PowerUpArrayList.size(); i++) {
-			System.out.println("P x :" + PowerUpArrayList.get(i).x + "P y: "
-					+ PowerUpArrayList.get(i).y);
-		}
-
+		//for (int i = 0; i < PowerUpArrayList.size(); i++) {
+		//	System.out.println("P x :" + PowerUpArrayList.get(i).x + "P y: "
+		//			+ PowerUpArrayList.get(i).y);
+		//}
+		
 		return Move.NONE;
 	}
 
@@ -83,18 +81,42 @@ public class PlayerAI extends ClientAI {
 
 	
 	// Check if player is safe in a given point
-	boolean isPlayerSafe(int currX, int currY, Gameboard gameboard) {
-		System.out.println("Checking is player is safe\n");
+	boolean isPlayerSafe(Point currPoint, Gameboard gameboard) {
 		// to be checked for danger
 		Point check = new Point();
-		// Check if there are turrets within the current pont
+		// Check if there are turrets within the current point
 		for (int i = 0; i < 4; i++) {
 			for (int r = 0; r < 5; r++) {
-				check.x = currX + r * diry[r];
-				check.y = currY + r * diry[r];
-				wrapAroundPoint(check);
+				check.x = currPoint.x + r * dirx[i];
+				check.y = currPoint.y + r * diry[i];
 				wrapAroundPoint(check);
 				
+				try {
+					// if we find a wall we don't have to check anymore in that direction. player is safe
+					if (gameboard.isWallAtTile(check.x,check.y)){
+						//System.out.println("wall");
+						break;
+					}
+					
+					// if we find a Turret, we have to check for some conditions
+					// Ex. Turret might be in its cool down time. That means the tile is safe
+					if (gameboard.isTurretAtTile(check.x,check.y)){
+						if( gameboard.getTurretAtTile(check.x,check.y).isFiringNextTurn() ){
+							return false;
+						}	
+						if( gameboard.getTurretAtTile(check.x,check.y).getFireTime() >= 1){
+							return false;
+						}
+						return false;
+					}
+					
+					// Check for bullets
+					
+					
+				} catch (MapOutOfBoundsException | NoItemException e) {
+					e.printStackTrace();
+				}
+					
 			}
 		}
 		return true;
